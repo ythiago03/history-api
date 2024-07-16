4d5a55650fe56eb3f973f143c65d1ccfccc54bb8
Thiago Henrique <thiagohcfidencio@gmail.com>
2024-07-16 19:38:23 -0300

    First commit

---
+++ src/test/java/com/gestao/api/history/HistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao.api.history;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
