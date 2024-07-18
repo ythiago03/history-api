772dc8ce0e4e5886656509a6a671085b4ff8bf78
Thiago Henrique <thiagohcfidencio@gmail.com>
2024-07-17 21:06:07 -0300

    fix: fix spring security unauthorized erro and add @autowired in repository

--- src/main/java/com/gestao/api/history/controllers/HistoryController.java
+++ src/main/java/com/gestao/api/history/controllers/HistoryController.java
@@ -2,6 +2,7 @@ package com.gestao.api.history.controllers;
 
 import com.gestao.api.history.models.dto.HistoryDTO;
 import com.gestao.api.history.services.HistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("history")
 public class HistoryController {
 
+    @Autowired
     private HistoryService historyService;
 
     @PostMapping
