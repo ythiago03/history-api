c7cff4cd8b36aa1c279f14c9b5e5ed86f9c59ffa
Thiago Henrique <thiagohcfidencio@gmail.com>
2024-07-29 20:54:35 -0300

    feat: block endpoints by roles

--- src/main/java/com/gestao/api/history/config/security/SecurityConfig.java
+++ src/main/java/com/gestao/api/history/config/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.gestao.api.history.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,10 @@ public class SecurityConfig {
         return  httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/history/*").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/history/filtered/*").hasAnyRole("ADMIN", "MANAGER")
+                )
                 .build();
     }
 }
